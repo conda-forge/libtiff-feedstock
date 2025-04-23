32267c7c0634a71f448ed9ac491bc289747368d8
Mark Harfouche <mark.harfouche@gmail.com>
2025-04-22 21:25:04 -0400

    Install diffutls

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,7 +18,7 @@ cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./config
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 ]]; then
-  make check
+  make check || (cat test/test-suite.log && exit 1)
 fi
 make install
 
